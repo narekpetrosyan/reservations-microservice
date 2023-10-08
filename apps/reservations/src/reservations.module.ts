5b80f23b29728a6dddbb47bdde89d8f1ab06a7fa
Narek Petrosyan <narekpetrosyan@Nareks-Laptop.local>
2023-10-08 18:45:29 +0400

    add validation

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { ReservationRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+    LoggerModule,
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationRepository],
+  exports: [],
+})
+export class ReservationsModule {}
